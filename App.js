1ecae4b5f4307d09cfe8342be82daf4be0925448
cmj0222 <chlalswp0222@gmail.com>
2020-12-01 09:08:42 +0900

    .

--- App.js
+++ App.js
@@ -1,21 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import Loading from "./Loading";
+import * as Location from 'expo-location'
+import {Alert} from 'react-native'
+import axios from 'axios'
+import Weather from './Weather'
+const API_KEY = '48f80f656ffc231ac5970a3793d7c5a8'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default class extends React.Component {
+  state = {
+    isLoading: true
+  };
+  getWeather = async (latitude, longitude) => {
+    const {
+      data: {
+        main: { temp },
+        weather
+      }
+    } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
+      );
+      this.setState({
+        isLoading: false,
+        condition: weather[0].main,
+        temp
+      });
+      console.log({weather})
+    };
+    getLocation = async () => {
+      try {
+        await Location.requestPermissionsAsync();
+        const {
+          coords: { latitude, longitude }
+        } = await Location.getCurrentPositionAsync();
+        this.getWeather(latitude, longitude);
+        
+      } catch (error) {
+        Alert.alert("Can't find you.", "So sad");
+      }
+    };
+    componentDidMount() {
+      this.getLocation();
+    }
+    render() {
+      const { isLoading, temp, condition } = this.state;
+      return isLoading ? (
+        <Loading />
+      ) : (
+        <Weather temp={Math.round(temp)} condition={condition} />
+      );
+    }
+  }
---
+++ Weather.js
@@ -0,0 +1,107 @@
+import React from "react";
+import { View, Text, StyleSheet, StatusBar } from "react-native";
+import PropTypes from "prop-types";
+import {MaterialCommunityIcons} from '@expo/vector-icons'
+import { LinearGradient } from 'expo-linear-gradient'
+
+const weatherOptions = {
+  Thunderstorm: {
+    iconName: "weather-lightning",
+    gradient: ["#373B44", "#4286f4"]
+  },
+  Drizzle: {
+    iconName: "weather-hail",
+    gradient: ["#89F7FE", "#66A6FF"]
+  },
+  Rain: {
+    iconName: "weather-rainy",
+    gradient: ["#00C6FB", "#005BEA"]
+  },
+  Snow: {
+    iconName: "weather-snowy",
+    gradient: ["#7DE2FC", "#B9B6E5"]
+  },
+  Atmosphere: {
+    iconName: "weather-hail",
+    gradient: ["#89F7FE", "#66A6FF"]
+  },
+  Clear: {
+    iconName: "weather-sunny",
+    gradient: ["#FF7300", "#FEF253"]
+  },
+  Clouds: {
+    iconName: "weather-cloudy",
+    gradient: ["#D7D2CC", "#304352"]
+  },
+  Mist: {
+    iconName: "weather-hail",
+    gradient: ["#4DA0B0", "#D39D38"]
+  },
+  Dust: {
+    iconName: "weather-hail",
+    gradient: ["#4DA0B0", "#D39D38"]
+  },
+  Haze: {
+    iconName: "weather-hail",
+    gradient: ["#4DA0B0", "#D39D38"],
+    title: "Haze",
+    subtitle: "Just don't go outside."
+  }
+};
+
+export default function Weather({ temp, condition }) {
+  return (
+      <LinearGradient
+        // Background Linear Gradient
+        colors={weatherOptions[condition].gradients}
+        style={styles.container
+        }
+      >
+      <StatusBar barStyle='default' />
+      <View style={styles.halfcontainer}>
+      <MaterialCommunityIcons 
+       name={weatherOptions["Clear"].iconName} size={80} color="white" />
+      <Text style={styles.temp}>{temp}</Text>
+      </View>
+      <View style={{...styles.halfcontainer, ...styles.textContainer}}>
+
+      </View>
+      </LinearGradient>
+  );
+}
+
+Weather.propTypes = {
+  temp: PropTypes.number.isRequired,
+  condition: PropTypes.oneOf([
+    "Thunderstorm",
+    "Drizzle",
+    "Rain",
+    "Snow",
+    "Atmosphere",
+    "Clear",
+    "Clouds",
+    "Haze",
+    "Mist",
+    "Dust"
+  ]).isRequired
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  temp : { 
+    fontSize: 35
+  },
+  halfcontainer: {
+    flex:1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  textContainer: {
+    paddingHorizontal:20,
+    alignItems: 'flex-start'
+  }
+});
